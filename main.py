f8408e3bd7646de508e4c20fb85d848ff03e1d81
junho <junhobang01@gmail.com>
2022-01-21 19:34:33 +0900

    number1

--- main.py
+++ main.py
@@ -11,5 +11,29 @@ def AND(x1, x2):
     else:
         return 1
 
-a = AND(1, 2)
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+a = AND(1, 0)
+print(a)
+a = NAND(1, 0)
+print(a)
+a = OR(1, 0)
 print(a)

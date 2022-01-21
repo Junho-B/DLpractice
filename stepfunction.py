118b86eadf7a439b7e02441690a7e266b9a2977c
junho <junhobang01@gmail.com>
2022-01-22 05:03:56 +0900

    step_function

--- stepfunction.py
+++ stepfunction.py
@@ -5,5 +5,6 @@ def step_function(x):
     y = x > 0 #비교 연산자이기 때문에 y에 bool값이 저장됨
     return y.astype(np.int32) #위의 bool값을 int형으로 바꿔줌
 
+print(step_function(np.array([1.0, -2.0, 3.0])))
 print(step_function(np.array([1.0, 2.0])))
 

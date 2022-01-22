e1da716ee883c15384a4531facd97371272950fa
junho <junhobang01@gmail.com>
2022-01-22 14:24:35 +0900

    functions

--- Neural_Network/sigmoid.py
+++ Neural_Network/sigmoid.py
@@ -4,8 +4,15 @@ import matplotlib.pylab as plt
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def relu(x):
+    return np.maximum(0,x)
+
 x = np.arange(-20.0, 20.0, 0.1)
 y = sigmoid(x)
-plt.plot(x,y)
-plt.ylim(-0.3,1.3)
+z = relu(x)
+plt.plot(x,y, label="sigmoid")
+plt.plot(x,z, linestyle="--", label="reLU")
+plt.ylim(-0.3,20.3)
+plt.legend()
 plt.show()
+
